08b5eb76d8e2255190207067376def793748c56b
Fernando Bahia <125662967+FernandoBahia@users.noreply.github.com>
2025-03-11 22:31:57 -0300

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,9 +7,9 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
           Nasci em Florianópolis, tenho 18 anos e me interesso por <span className="purple">Técnologia</span> desde pequeno, terminei o ensino médio integrado com o Curso Técnico de <b className="purple">Análise e Desenvolvimento de Sistemas</b> no ano de 2024 e
-          pretendo cursar Ciências da Computação para ampliar cada vez mais meus conhecimentos. &nbsp;
+          comecei minha graduação de nível superior no ano de 2025 também em ADS. &nbsp;
             <br />
-            No mesmo ano comecei a trabalhar na <b className="purple">Softplan</b> como jovem aprendiz de Infraestrutura de TI. Gosto muito de Desenvolvimento Web, Banco de dados, Cyber Segurança, Redes e Inteligência Artificial            <br />
+            Em 2024 também comecei a trabalhar na <b className="purple">Softplan</b> como jovem aprendiz de Infraestrutura de TI. Tenho uma paixão por desenvolvimento de Software, web, banco de dados, IA e Redes.           <br />
             <br />
             <br />
           </p>
